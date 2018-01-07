fc6f102d85074d42261da448e46fdff559d14aa6
sa-mori <pillows77@gmail.com>
2018-01-07 21:09:04 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :post_images,only:[:new,:create,:index,:show,:destroy] do
+    resource :post_comments,only:[:create,:destroy]
+    resource :favorites,only:[:create,:destroy]
+  end
+  root 'post_images#index'
+  resources :users,only:[:show,:edit,:update]
+
+end
